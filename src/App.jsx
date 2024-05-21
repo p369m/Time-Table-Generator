c8dc6cf5046bd996f115eaaae302236781c7a1b5
Partha Mandal <parthamandal621@gmail.com>
2024-05-21 23:01:39 +0530

    Contact details added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Contact from "./Contact";
 import RoutineMakerApp from "./RoutineMakerApp";
 
 function App() {
   return (
     <>
       <RoutineMakerApp />
+      <Contact/>
     </>
   );
 }
---
+++ src/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Contact.css';
+
+const Contact = () => {
+  return (
+    <div className='contact'>
+      <a href="https://github.com/p369m/Time-Table-Generator" target="_blank" rel="noopener noreferrer">GitHub</a>
+      <a href="mailto:parthamandal621@gmail.com">Mail me</a>
+      <a href="https://linkedin.com/in/pm369/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+      <p>Phone: +91-7001356148</p>
+    </div>
+  );
+};
+
+export default Contact;
