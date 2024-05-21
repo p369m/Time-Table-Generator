78e4d650b91114332b2ecf3f7021d6cc3e086550
Partha Mandal <parthamandal621@gmail.com>
2024-05-21 23:41:47 +0530

    modified package.json for render 2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: process.env.PORT || 5173,
+    host: true, // to make the server accessible externally
+  },
+});
